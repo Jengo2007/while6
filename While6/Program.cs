8e83f1d2a9a1949121c92bf8272abbaa4d7f3ec9
Jengo2007 <zuyunovmagamed@gmail.com>
2024-12-02 19:04:13 +0300

    while6

---
+++ While6/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+var factorial=1.0;
+while (N > 0)
+{
+    factorial *= N;
+    N -= 2;
+}
+
+Console.WriteLine(factorial);
+
